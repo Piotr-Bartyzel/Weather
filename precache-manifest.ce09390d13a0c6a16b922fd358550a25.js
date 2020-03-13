b146494f6460defe169ebe4e3aeec8af4da8a273
Piotr <pbartyzel@interia.eu>
2020-03-13 23:40:06 +0100

    Updates

---
+++ precache-manifest.ce09390d13a0c6a16b922fd358550a25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfe79ef5f88e4b6fa8ba7b49e8fa5fc",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "688af0aede46935cf42e",
+    "url": "/Weather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0865413cc01cc44ca9b5",
+    "url": "/Weather/static/css/main.3add4da8.chunk.css"
+  },
+  {
+    "revision": "688af0aede46935cf42e",
+    "url": "/Weather/static/js/2.9f445d1e.chunk.js"
+  },
+  {
+    "revision": "c5a360e3ba18abe281bb155a87949c85",
+    "url": "/Weather/static/js/2.9f445d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0865413cc01cc44ca9b5",
+    "url": "/Weather/static/js/main.d9eab119.chunk.js"
+  },
+  {
+    "revision": "e53db7ad8441773a10db",
+    "url": "/Weather/static/js/runtime-main.8025d321.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
